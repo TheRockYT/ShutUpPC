35ae6db2228df90e356f5d14b9da156d22129f41
TheRockYT <therockyt.kanal@gmail.com>
2022-10-12 22:37:23 +0200

    Version 1.0.2 is out
    Signed-off-by: TheRockYT <therockyt.kanal@gmail.com>

--- src/main/kotlin/therockyt/shutuppc/GuiInterface.kt
+++ src/main/kotlin/therockyt/shutuppc/GuiInterface.kt
@@ -1,6 +1,6 @@
 package therockyt.shutuppc
 
-import com.azul.crs.client.models.Network
+import com.google.gson.JsonParser
 import sendPacket
 import java.awt.Dimension
 import java.awt.GridLayout
@@ -9,6 +9,12 @@ import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.URL
+import java.net.URLConnection
+import java.util.stream.Collectors
 import javax.swing.*
 import javax.swing.event.ListSelectionListener
 import javax.swing.table.DefaultTableModel
@@ -16,11 +22,18 @@ import kotlin.system.exitProcess
 
 
 class GuiInterface : ActionListener, KeyAdapter() {
-    val version = "1.0.1"
+    val version = "1.0.0"
     private var frame = JFrame("ShutUpPC by TheRockYT")
     private var jtable: JTable? = null
     private var pctable: PcConfigTable? = null
     var tablemodel: DefaultTableModel? = null
+    fun setAction(s: String?){
+        if(s == null){
+            frame.title = "ShutUpPC by TheRockYT"
+        }else{
+            frame.title = "$s - ShutUpPC by TheRockYT"
+        }
+    }
     fun create(){
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         frame.isResizable = false
@@ -46,10 +59,13 @@ class GuiInterface : ActionListener, KeyAdapter() {
         val process = JMenu("Process")
         val about = JMenuItem("About")
         val quit = JMenuItem("Quit")
+        val updates = JMenuItem("Check for Updates (API)")
         about.addActionListener(this)
         quit.addActionListener(this)
+        updates.addActionListener(this)
         process.add(about)
         process.add(quit)
+        process.add(updates)
         menuBar.add(process)
         val strArr = ArrayList<String>()
         strArr.add("STATE")
@@ -114,6 +130,64 @@ class GuiInterface : ActionListener, KeyAdapter() {
             JOptionPane.showMessageDialog(frame, "Delete failed!\nNo PC selected!", "ShutUpPC - Error", JOptionPane.ERROR_MESSAGE)
         }
     }
+    fun checkForUpdates(){
+        setAction("Checking for updates...")
+        try {
+            val content = getURLContent(URL("https://www.therockyt.com/api/shutuppc/versions.json"))
+            val jsonElement = JsonParser().parse(content)
+            val jsonObject = jsonElement.asJsonObject
+            val latest = jsonObject["latest-release"].asString
+            val latest_development = jsonObject["latest-development"].asString
+            val current = jsonObject["version"].asJsonObject[version].asJsonObject
+            var msg = "You are using ShutUpPC v$version"
+            var warn = false
+            if(version.equals(latest, true)){
+                msg += "\nThe latest release"
+            }else
+            if(version.equals(latest_development, true)){
+                msg += "\nThe latest development version"
+            }else{
+
+                if(current["development"].asBoolean){
+                    msg += "\nAn outdated development version."
+                    warn = true
+                }else{
+                    msg += "\nAn outdated release."
+                }
+                msg += "\n(latest release: v$latest/latest development: $latest_development)"
+                if(current["update"].asBoolean){
+                    msg += "\nAn update is recommended."
+                    warn = true
+                }
+                msg += "\nUpdate at https://www.therockyt.com/shutuppc."
+            }
+            if(warn){
+
+                JOptionPane.showMessageDialog(frame, msg, "ShutUpPC - Update", JOptionPane.WARNING_MESSAGE)
+            }else{
+                JOptionPane.showMessageDialog(frame, msg, "ShutUpPC - Update", JOptionPane.INFORMATION_MESSAGE)
+
+            }
+        } catch (e: Exception) {
+            JOptionPane.showMessageDialog(frame, "ShutUpPC failed to load updates.\n${e.message}", "ShutUpPC - Update failed", JOptionPane.ERROR_MESSAGE)
+        }
+        setAction(null)
+    }
+
+    @Throws(Exception::class)
+    fun getURLContent(url: URL): String? {
+        val connection = url.openConnection()
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+        val `is` =  connection.getInputStream()
+        val reader = BufferedReader(InputStreamReader(`is`))
+        var lines: String? = ""
+        var line: String? = null
+        while (reader.readLine().also { line = it } != null) {
+            lines += line
+        }
+        reader.close()
+        return lines
+    }
     fun wakeCurrentPC(){
         val row = jtable!!.selectedRow
         if(row > -1){
@@ -155,6 +229,9 @@ class GuiInterface : ActionListener, KeyAdapter() {
                     }else{
                         JOptionPane.showMessageDialog(frame, "Edit failed!\nNo PC selected!", "ShutUpPC - Error", JOptionPane.ERROR_MESSAGE)
                     }
+                }
+                if(src.text.equals("Check for Updates (API)")){
+                    checkForUpdates()
                 }
             }
         }
